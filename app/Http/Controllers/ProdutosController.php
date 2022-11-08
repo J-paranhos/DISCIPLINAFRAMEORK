d29ac9b607eed3432636b420167b9633d74da6f4
JEAN PARANHOS <j.lucasparanhos@gmail.com>
2022-11-08 11:31:06 -0300

    ATIVIDADE PRONTA

---
+++ app/Http/Controllers/ProdutosController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use produtos;
+
+class ProdutosController extends Controller
+{
+    
+    private $produtos = [
+        [
+            'nome' => 'Processador Intel I9',
+            'categoria' => 'Informática',
+            'preco' => 1899.90,
+            'novo' => true,
+            'promocao' => false
+        ],
+        [
+            'nome' => 'Guitarra Fender Stratocaster',
+            'categoria' => 'Instrumentos musicais',
+            'preco' => 9899.90,
+            'novo' => true,
+            'promocao' => false
+        ],
+        [
+            'nome' => 'TV Sony 59"',
+            'categoria' => 'Eletrônicos',
+            'preco' =>  2499.90,
+            'novo' => true,
+            'promocao' => true
+        ],
+        [
+            'nome' => 'Soundbar LG XPTO',
+            'categoria' => 'Eletrônicos',
+            'preco' =>  899.90,
+            'novo' => true,
+            'promocao' => true
+        ],
+        [
+            'nome' => 'Processador AMD Ryzen 3',
+            'categoria' => 'Informática',
+            'preco' => 399.90,
+            'novo' => false,
+            'promocao' => false
+        ],
+        [
+            'nome' => 'iPhone 8 - 64GB',
+            'categoria' => 'Celulares',
+            'preco' =>  1899.90,
+            'novo' => false,
+            'promocao' => false
+        ],
+        [
+            'nome' => 'Smartphone Samsung Galaxy S20',
+            'categoria' => 'Celulares',
+            'preco' =>  1299.90,
+            'novo' => false,
+            'promocao' => true
+        ]
+    ];
+    
+    private function Tipo($novo)
+    {
+
+       $RetornoLista = [];
+       $indice=1;
+       foreach($this->produtos as $protudo)
+       {
+         
+         $id = $protudo['id'] = $indice;
+         array_push ($protudo,$protudo['id']);     
+         $protudo['preco'] = 'R$ ' . number_format($protudo['preco'], 2, ',', '.');
+
+        if ($protudo['novo'] == $novo)
+        {
+
+            array_push ($RetornoLista,$protudo);
+        }
+
+
+
+        $indice+=1; 
+       }
+
+       return $RetornoLista;
+       
+    }
+
+
+    public function ListaTodos()
+    {
+
+         return $this->produtos;
+    }
+
+
+
+    public function Novos()
+    {        
+
+        
+        $produtos = $this->Tipo(true);
+
+        return view('novos',compact('produtos'));
+
+    }
+
+    public function Usados()
+    {   $produtos = $this->Tipo(false);
+        return view('usados',compact('produtos'));
+    }
+
+}
---
+++ resources/views/novos.blade.php
@@ -0,0 +1,37 @@
+@extends('base')
+ 
+@section('title', 'Novos')
+
+ 
+@section('contuedo')
+<div  class="row"> <h2> Produtos Novos </h2> </div>
+
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col" >Produto</th>
+        <th scope="col">Categoria</th>
+        <th scope="col">Preço</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($produtos as $produto)
+
+        @if ($produto['promocao'] == true) 
+        <tr  class="table-success">
+        @else
+        <tr>
+        @endif    
+        <th scope="row"> {{ $produto['id'] }} </th>
+        <td>{{ $produto['nome'] }}</td>
+        <td>{{ $produto['categoria'] }}</td>
+        <td>{{ $produto['preco'] }}</td>
+
+      </tr>
+
+    @endforeach
+    </tbody>
+  </table>
+  
+@stop
