31639af22e5a2202fee5d0911ee697680fa0079a
JEAN PARANHOS <j.lucasparanhos@gmail.com>
2022-11-08 11:43:51 -0300

    ADICIONAR TODOS

---
+++ resources/views/todos.blade.php
@@ -0,0 +1,36 @@
+@extends('base')
+ 
+@section('title', 'Todos')
+
+ 
+@section('contuedo')
+<div class="row"> <h2> Produtos Todos </h2> </div>
+
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col" >Produto</th>
+        <th scope="col">Categoria</th>
+        <th scope="col">Preço</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($produtos as $produto)
+        @if ($produto['promocao'] == true) 
+        <tr  class="table-success">
+        @else
+        <tr>
+        @endif    
+        <th scope="row"> {{ $produto['id'] }} </th>
+        <td>{{ $produto['nome'] }}</td>
+        <td>{{ $produto['categoria'] }}</td>
+        <td>{{ $produto['preco'] }}</td>
+
+      </tr>
+
+    @endforeach
+    </tbody>
+  </table>
+  
+@stop
